2e0de759c691c63005c2c7b5ea3eeccf1f18cef9
passerby223 <1912315910@qq.com>
2021-06-09 00:19:03 +0800

    优化docker-compose容器编排部署

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM python:3.8
-MAINTAINER anonymous
 WORKDIR /root/beer_server
 COPY . .
-RUN apt update && apt upgrade -y && pip3 install -i https://pypi.douban.com/simple -U pip \
+RUN apt update \
+    && apt install -y netcat \
+    && pip3 install -i https://pypi.douban.com/simple -U pip \
     && pip3 install -i https://pypi.douban.com/simple -r requirements.txt \
-    && python3 manage.py makemigrations \
-    && python3 manage.py migrate \
-    && python3 manage.py init_admin \
-    && python3 manage.py update_global_function_content
-CMD ["gunicorn", "--workers=3", "beer_server.wsgi"]
+    && chmod +x ./start_server.sh
+CMD ["/bin/bash", "./start_server.sh"]
---
+++ start_server.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# 1. 等待MySQL服务启动后再进行数据迁移。nc(即netcat缩写)端口扫描
+# 2. 收集静态文件到根目录static文件夹
+# 3. 进行数据迁移并初始化管理员数据和全局函数文件数据
+# 5. 用 gunicorn 启动 django 服务
+
+# 获取当前容器CPU核数
+cpu_core_nums=$(cat /proc/cpuinfo | grep "cores" | uniq | awk '{print $4}')
+
+while ! nc -z mysql 3306 ; do
+    echo "正在等待MySQL服务启动..."
+    sleep 3
+done
+
+echo "MySQL服务已启动完毕。"
+
+while ! nc -z rabbitmq 5672 ; do
+    echo "正在等待rabbitmq服务启动..."
+    sleep 3
+done
+
+echo "rabbitmq服务已启动完毕。即将开始部署Django项目。"
+
+python3 manage.py collectstatic --noinput \
+  && echo "收集静态文件完毕。" \
+  && python3 manage.py makemigrations \
+  && python3 manage.py migrate \
+  && echo "项目数据迁移完毕。" \
+  && python3 manage.py init_admin \
+  && python3 manage.py update_global_function_content \
+  && echo "初始化项目数据完毕。" \
+  && nohup celery -A beer_server worker -l INFO >> celery.log 2>&1 \& \
+  && echo "启动Celery异步任务队列服务完毕。" \
+  && gunicorn --workers=$((cpu_core_nums * 2 + 1)) --bind=0.0.0.0 beer_server.wsgi
