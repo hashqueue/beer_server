469aa7a1a17dc446c22cb41faeab05c43b6295b8
passerby223 <1912315910@qq.com>
2021-06-10 01:17:01 +0800

    try bugfix1

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,8 @@ RUN mv /etc/apt/sources.list /etc/apt/sources.list.bak \
     && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-backports main contrib non-free" >> /etc/apt/sources.list \
     && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian-security buster/updates main contrib non-free" >> /etc/apt/sources.list \
     && apt update \
-    && apt install -y netcat
-CMD ["/sh", "./deploy_server.sh"]
+    && apt install -y netcat \
+    && pip3 install -i https://pypi.douban.com/simple -U pip \
+    && pip3 install -i https://pypi.douban.com/simple -r requirements.txt \
+    && python3 manage.py collectstatic --noinput \
+    && echo "收集静态文件完毕。"
--- deploy_server.sh
+++ deploy_server.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # 获取当前容器CPU核数
-cpu_core_nums=$(cat /proc/cpuinfo | grep "cores" | uniq | awk '{print $4}')
-
-pip3 install -i https://pypi.douban.com/simple -U pip \
-    && pip3 install -i https://pypi.douban.com/simple -r requirements.txt \
-    && python3 manage.py collectstatic --noinput \
-    && echo "收集静态文件完毕。"
+# cpu_core_nums=$(cat /proc/cpuinfo | grep "cores" | uniq | awk '{print $4}')
 
 while ! nc -z mysql 3306 ; do
     echo "正在等待MySQL服务启动..."
@@ -22,11 +17,3 @@ done
 echo "rabbitmq服务已启动完毕。"
 nohup celery -A beer_server worker -l INFO >> celery.log 2>&1 &
 echo "启动Celery异步任务队列服务完毕。即将开始部署Django项目。"
-
-python3 manage.py makemigrations \
-    && python3 manage.py migrate \
-    && echo "项目数据迁移完毕。" \
-    && python3 manage.py init_admin \
-    && python3 manage.py update_global_function_content \
-    && echo "初始化系统管理员数据和全局函数文件完毕。" \
-    && gunicorn --workers=$((cpu_core_nums * 2 + 1)) --bind=0.0.0.0 beer_server.wsgi
