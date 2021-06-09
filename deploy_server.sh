f8899636d407644c00fbd91679dce6e3afcf6b9b
passerby223 <1912315910@qq.com>
2021-06-10 02:22:22 +0800

    try bugfix2

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # 获取当前容器CPU核数
-# cpu_core_nums=$(cat /proc/cpuinfo | grep "cores" | uniq | awk '{print $4}')
+cpu_core_nums=$(cat /proc/cpuinfo | grep "cores" | uniq | awk '{print $4}')
+
+export PYTHONUNBUFFERED=1
+export DJANGO_SETTINGS_MODULE=beer_server.settings
 
 while ! nc -z mysql 3306 ; do
     echo "正在等待MySQL服务启动..."
@@ -17,3 +20,12 @@ done
 echo "rabbitmq服务已启动完毕。"
 nohup celery -A beer_server worker -l INFO >> celery.log 2>&1 &
 echo "启动Celery异步任务队列服务完毕。即将开始部署Django项目。"
+
+source venv/bin/activate \
+    && python3 manage.py makemigrations \
+    && python3 manage.py migrate \
+    && echo "项目数据迁移完毕。" \
+    && python3 manage.py init_admin \
+    && python3 manage.py update_global_function_content \
+    && echo "初始化系统管理员数据和全局函数文件完毕。" \
+    && gunicorn --workers=$((cpu_core_nums * 2 + 1)) --bind=0.0.0.0 beer_server.wsgi
