86abe1db2063e187d514ab53b12a1ac093425e55
passerby223 <1912315910@qq.com>
2021-06-15 00:27:53 +0800

    使用github pages托管项目平台文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    base: "/beer_server/",
+    title: 'Beer接口自动化测试平台',
+    description: '基于Django和Vue开发的接口自动化测试平台',
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+    sidebar: [
+      ['/', '首页'],
+      ['/用户与权限', '用户与权限'],
+      ['/项目管理', '项目管理'],
+      ['/套件管理', '套件管理'],
+    ]
+  }
+}
